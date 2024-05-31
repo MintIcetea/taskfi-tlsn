2c3e2508cb490ce91813d4ca0f662a07ac2b55f6
Minh Hoang Tien <minh.hoang@icetea.io>
2024-05-31 14:55:31 +0700

    feat: make notarize request return early and start background process

--- src/routes/notarize.rs
+++ src/routes/notarize.rs
@@ -1,11 +1,14 @@
+use std::str::FromStr;
+
 use crate::{
+    config::read_config,
     errors::ServerError,
     notary::{run_notary, DEFAULT_MAX_RECV_LIMIT, DEFAULT_MAX_SENT_LIMIT},
     r2::R2Manager,
 };
 use actix_web::{
     http::{header::HeaderMap, Method},
-    web, HttpRequest, HttpResponse,
+    HttpRequest, HttpResponse,
 };
 use hyper::{Request, StatusCode};
 use hyper_util::rt::TokioIo;
@@ -21,20 +24,17 @@ struct NotarizeResponse {
 }
 
 #[derive(serde::Deserialize, serde::Serialize)]
-struct NotarizeHeaders<'a> {
-    id: &'a str,
-    host: &'a str,
-    path: &'a str,
-    method: &'a str,
+struct NotarizeHeaders {
+    id: String,
+    host: String,
+    path: String,
+    method: String,
 }
 
 pub async fn handle_notarize_v2(
-    data: web::Data<R2Manager>,
     request: HttpRequest,
     bytes: actix_web::web::Bytes,
 ) -> HttpResponse {
-    let r2 = data.clone();
-
     let headers = request.headers();
     let notarize_headers = match extract_headers(headers) {
         Ok(headers) => headers,
@@ -73,6 +73,31 @@ pub async fn handle_notarize_v2(
     };
     info!("Body extracted {}", json!(request_body).to_string());
 
+    let request_id = notarize_headers.id.clone().to_string();
+
+    tokio::spawn(background_notarize(
+        notarize_headers,
+        String::from(auth_header),
+        request_body.clone(),
+    ));
+
+    HttpResponse::Accepted()
+        .body(serde_json::to_string(&NotarizeResponse { id: request_id }).unwrap())
+}
+
+async fn background_notarize(headers: NotarizeHeaders, auth_header: String, request_body: String) {
+    let proofs = notarize(&headers, auth_header.clone(), request_body.clone()).await;
+
+    let file_name = format!("{}.json", headers.id);
+
+    store_proofs(
+        file_name.as_str(),
+        serde_json::to_string(&proofs).unwrap().as_bytes(),
+    )
+    .await;
+}
+
+async fn notarize(headers: &NotarizeHeaders, auth_header: String, request_body: String) -> String {
     let (prover_socket, notary_socket) = tokio::io::duplex(1 << 16);
 
     // Start a local simple notary service
@@ -83,7 +108,7 @@ pub async fn handle_notarize_v2(
         .id("example")
         .max_recv_data(DEFAULT_MAX_RECV_LIMIT)
         .max_sent_data(DEFAULT_MAX_SENT_LIMIT)
-        .server_dns(notarize_headers.host)
+        .server_dns(headers.host.clone())
         .build()
         .unwrap();
 
@@ -97,7 +122,7 @@ pub async fn handle_notarize_v2(
 
     info!("Init TLS connection from client to server");
     // Connect to the Server via TCP. This is the TLS client socket.
-    let client_socket = tokio::net::TcpStream::connect((notarize_headers.host, 443))
+    let client_socket = tokio::net::TcpStream::connect((headers.host.clone(), 443))
         .await
         .unwrap();
 
@@ -121,17 +146,11 @@ pub async fn handle_notarize_v2(
     tokio::spawn(connection);
 
     // Build a simple HTTP request with common headers
-    info!(
-        "Requesting to https://{}/{}",
-        notarize_headers.host, notarize_headers.path
-    );
+    info!("Requesting to https://{}/{}", headers.host, headers.path);
     let request_builder = Request::builder()
-        .uri(format!(
-            "https://{}/{}",
-            notarize_headers.host, notarize_headers.path
-        ))
-        .method(notarize_headers.method)
-        .header("Host", notarize_headers.host)
+        .uri(format!("https://{}/{}", headers.host, headers.path))
+        .method(hyper::Method::from_str(&headers.method).unwrap())
+        .header("Host", headers.host.clone())
         .header("Authorization", format!("Bearer {}", auth_header))
         .header("Accept", "*/*")
         .header("User-Agent", "TaskFi ID")
@@ -141,18 +160,14 @@ pub async fn handle_notarize_v2(
     let response = match request_sender.send_request(request_builder).await {
         Ok(response) => response,
         Err(_) => {
-            return HttpResponse::InternalServerError().body(
-                json!(ServerError::new(
-                    format!("Request to {} failed", notarize_headers.host).as_str()
-                ))
-                .to_string(),
-            )
+            tracing::error!("Request to {} failed", headers.host);
+            return String::from("");
         }
     };
     assert!(response.status() == StatusCode::OK);
     info!(
         "Request to https://{}/{} came back OK",
-        notarize_headers.host, notarize_headers.path
+        headers.host, headers.path
     );
 
     // The Prover task should be done now, so we can grab the Prover.
@@ -162,23 +177,28 @@ pub async fn handle_notarize_v2(
     let prover = prover.start_notarize();
     let proof = build_proof_without_redactions(prover).await;
 
-    let file_name = format!("{}.json", notarize_headers.id);
+    serde_json::to_string(&proof).unwrap()
+}
+
+async fn store_proofs(file_name: &str, proofs: &[u8]) {
+    // Connect to R2 with app config
+    let app_config = read_config().unwrap_or_else(|err| {
+        panic!(
+            "Failed to read application configurations with errors {:?}",
+            err
+        )
+    });
+
+    let r2 = R2Manager::new(&app_config.r2).await;
 
     info!("Start uploading {} to R2", file_name);
     r2.upload(
-        file_name.as_str(),
-        serde_json::to_string(&proof).unwrap().as_bytes(),
+        file_name,
+        proofs,
         Some("immutable"),
         Some("multipart/form-data"),
     )
     .await;
-
-    HttpResponse::Ok().body(
-        serde_json::to_string(&NotarizeResponse {
-            id: notarize_headers.id.to_string(),
-        })
-        .unwrap(),
-    )
 }
 
 async fn build_proof_without_redactions(mut prover: Prover<Notarize>) -> TlsProof {
@@ -221,7 +241,7 @@ fn extract_headers(headers: &HeaderMap) -> Result<NotarizeHeaders, ServerError> 
     })
 }
 
-fn extract_header<'a>(headers: &'a HeaderMap, key: &str) -> Result<&'a str, ServerError<'a>> {
+fn extract_header<'a>(headers: &'a HeaderMap, key: &str) -> Result<String, ServerError<'a>> {
     let header = match headers.get(key) {
         Some(header) => header,
         None => {
@@ -230,7 +250,7 @@ fn extract_header<'a>(headers: &'a HeaderMap, key: &str) -> Result<&'a str, Serv
     };
 
     match header.to_str() {
-        Ok(header) => Ok(header),
+        Ok(header) => Ok(String::from(header)),
         Err(err) => {
             println!("Cannot parsed {} header with error {:?}", key, err);
             return Err(ServerError::new("header invalid"));
