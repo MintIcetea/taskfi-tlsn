daa375089c8bbc010080040a94317d53c41b556f
Hoang Tien Minh <minh.hoang@icetea.io>
2024-06-05 10:38:24 +0700

    feat: update logger with env_filter

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,11 @@
 use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpServer};
+use tracing::subscriber::set_global_default;
 use tracing_actix_web::TracingLogger;
+use tracing_bunyan_formatter::BunyanFormattingLayer;
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 use crate::{
     r2::R2Manager,
@@ -9,10 +13,12 @@ use crate::{
 };
 
 pub async fn run(listener: TcpListener, r2: R2Manager) -> Result<Server, std::io::Error> {
-    // tracing::subscriber::set_global_default(subscriber).expect("Failed to init tracing");
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO)
-        .init();
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let format_layer = BunyanFormattingLayer::new("taskfi-tlsn".to_string(), std::io::stdout);
+    let tracing_register = Registry::default().with(env_filter).with(format_layer);
+
+    LogTracer::init().expect("Failed to init logger");
+    set_global_default(tracing_register).expect("Failed to set subscriber");
 
     let server = HttpServer::new(move || {
         App::new()
